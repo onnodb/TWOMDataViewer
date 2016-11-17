b22c1e6f910cdfc9c1ddfa15d6851150df77b279
Onno Broekmans <onnodb@gmail.com>
2016-11-17 21:35:40 +0100

    Basic directory navigation

--- UI/TWOMDataViewer.m
+++ UI/TWOMDataViewer.m
@@ -3,6 +3,9 @@ classdef TWOMDataViewer < handle
 
     properties
 
+        % Currently shown directory.
+        currentDir;
+
     end
 
     % ------------------------------------------------------------------------
@@ -21,6 +24,7 @@ classdef TWOMDataViewer < handle
         function [this] = TWOMDataViewer(c)
             % Set up GUI.
             this.createGui();
+            this.browseTo(pwd());
         end
 
     end
@@ -46,11 +50,67 @@ classdef TWOMDataViewer < handle
             % ----- Menu
             % + File
             this.gui.menu.file = uimenu(this.gui.window, 'Label', 'File');
+            this.gui.menu.file_browse = uimenu(this.gui.menu.file, ...
+                  'Label',          'Browse...' ...
+                , 'Callback',       @(h,e) this.onBrowseBtnClick(h, e) ...
+                );
             this.gui.menu.file_exit = uimenu(this.gui.menu.file, ...
-                'Label', 'Exit' ...
-              , 'Callback', @(h, e) this.onFileExit(h, e) ...
+                  'Separator',      'on' ...
+                , 'Label',          'Exit' ...
+                , 'Callback',       @(h,e) this.onFileExit(h, e) ...
               );
 
+            % ----- Main columns
+            this.gui.root = uiextras.HBoxFlex('Parent', this.gui.window);
+            this.gui.root.Spacing = 5;
+
+            % ----- Left panel
+            this.gui.left.panel = uiextras.VBox('Parent', this.gui.root);
+
+            this.gui.dirpanel.panel = uiextras.HBox('Parent', this.gui.left.panel);
+            this.gui.dirpanel.edit = uicontrol(...
+                  'Parent',         this.gui.dirpanel.panel ...
+                , 'Style',          'edit' ...
+                , 'String',         '' ...
+                , 'Callback',       @(h,e) this.onDirChange ...
+                );
+            this.gui.dirpanel.browseBtn = uicontrol(...
+                  'Parent',         this.gui.dirpanel.panel ...
+                , 'Style',          'pushbutton' ...
+                , 'String',         'Browse...' ...
+                , 'Callback',       @(h,e) this.onBrowseBtnClick ...
+                );
+            this.gui.dirpanel.panel.Sizes = [-1 70];
+
+            this.gui.dirlisting = uicontrol(...
+                  'Parent',         this.gui.left.panel ...
+                , 'Style',          'listbox' ...
+                , 'Callback',       @(h,e) this.onDirListingChange ...
+                );
+
+            this.gui.left.panel.Sizes = [20 -1];
+
+            % ----- Center, main panel
+            this.gui.main.panel = uiextras.VBox('Parent', this.gui.root);
+
+            this.gui.root.Sizes         = [screenSize(3)/6 -1];
+            this.gui.root.MinimumWidths = [200 200];
+
+        end
+
+        function browseTo(this, newDir)
+            if exist(newDir, 'dir') == 7
+                this.currentDir = newDir;
+                this.gui.dirpanel.edit.String = newDir;
+                this.refreshDirectory();
+            else
+                errordlg(['Directory ' newDir ' not found']);
+                this.gui.dirpanel.edit.String = this.currentDir;
+            end
+        end
+
+        function refreshDirectory(this)
+            % TODO
         end
 
     end
@@ -60,6 +120,21 @@ classdef TWOMDataViewer < handle
     methods (Access=private)
 
         % ----- CALLBACK METHODS
+
+        function onBrowseBtnClick(this, ~, ~)
+            newDir = uigetdir(this.currentDir);
+            if newDir ~= 0
+                this.browseTo(newDir);
+            end
+        end
+
+        function onDirChange(this, ~, ~)
+            this.browseTo(this.gui.dirpanel.edit.String);
+        end
+
+        function onDirListingChange(this, ~, ~)
+            % TODO
+        end
 
         function onFileExit(this, ~, ~)
             delete(this.gui.window);
