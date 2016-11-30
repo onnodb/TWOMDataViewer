253ceb303afe1c8a7faa94795c2949fb06ce9795
Onno Broekmans <onnodb@gmail.com>
2016-11-30 22:06:57 +0100

    TWOMDataFile: Fetch properties by real name
    Make the "getTdmsProperty" method public, and let it fetch properties by
    their actual name, instead of the MATLAB varname-mangled name.

--- DataClasses/TWOMDataFile.m
+++ DataClasses/TWOMDataFile.m
@@ -52,6 +52,82 @@ classdef TWOMDataFile < handle
             end
         end
 
+        function [val] = getTdmsProperty(self, path, default, varargin)
+            % GETTDMSPROPERTY Get a single TDMS property value
+            %
+            % SYNTAX:
+            % val = tdf.getTdmsProperty(path)
+            % val = tdf.getTdmsProperty(path, default)
+            % val = tdf.getTdmsProperty(path, default, Key, Value, ___)
+            %
+            % INPUT:
+            % path = name of the property, if necessary prefixed by group
+            %       and channel name, using '\' as a separator.
+            %       For example 'Group\Channel\Property', 'Group\Property',
+            %       or, for a property in the root of the file, simply
+            %       'Property'.
+            % default = what value to return if the property does not exist.
+            %       If not given, and the property is not found, [] is returned.
+            %
+            % OUTPUT:
+            % val = property value.
+            %
+            % KEY-VALUE PAIR ARGUMENTS:
+            % CaseSensitive = whether or not group, channel and property names
+            %       are case sensitive (default: false)
+
+            if nargin < 3
+                default = [];
+            end
+            defArgs = struct(...
+                              'CaseSensitive',          false ...
+                            );
+            if nargin > 3
+                args = parseArgs(varargin, defArgs, {'CaseSensitive'});
+            else
+                args = defArgs;
+            end
+
+            if args.CaseSensitive
+                caseCmpFun = @strcmp;
+            else
+                caseCmpFun = @strcmpi;
+            end
+            val = default;
+
+            pathParts = strsplit(path, '\');
+            switch length(pathParts)
+                case 1
+                    groupName   = '';
+                    channelName = '';
+                    propName    = pathParts{1};
+                case 2
+                    groupName   = pathParts{1};
+                    channelName = '';
+                    propName    = pathParts{2};
+                case 3
+                    groupName   = pathParts{1};
+                    channelName = pathParts{2};
+                    propName    = pathParts{3};
+                otherwise
+                    error('Invalid parameter "path"');
+            end
+
+            for i = 1:length(self.tdmsMeta.rawDataInfo)
+                if caseCmpFun(self.tdmsMeta.groupNames{i}, groupName) ...
+                        && caseCmpFun(self.tdmsMeta.chanNames{i}, channelName)
+                    % Match group/channel, now find property
+                    for j = 1:length(self.tdmsMeta.rawDataInfo(i).propNames)
+                        if caseCmpFun(self.tdmsMeta.rawDataInfo(i).propNames{j}, propName)
+                            % Found it!
+                            val = self.tdmsMeta.rawDataInfo(i).propValues{j};
+                            return;
+                        end
+                    end
+                end
+            end
+        end
+
         function [valid] = isValidTWOMDataFile(self)
             valid = ~isempty(self.FileFormatVersion) ...
                     && isfield(self.tdmsStruct, 'FD_Data');
@@ -64,12 +140,12 @@ classdef TWOMDataFile < handle
     methods   % property getters
 
         function [val] = get.DateTime(self)
-            s = self.getTdmsProperty('Date_time');
+            s = self.getTdmsProperty('Date/time');
             val = datetime(s, 'InputFormat', 'yyyyMMdd-HHmmss');
         end
 
         function [val] = get.FileFormatVersion(self)
-            val = uint8(sscanf(self.getTdmsProperty('File_Format_Version'), '%u'));
+            val = uint8(sscanf(self.getTdmsProperty('File Format Version'), '%u'));
         end
 
         function [val] = get.ID(self)
@@ -81,28 +157,6 @@ classdef TWOMDataFile < handle
     % ------------------------------------------------------------------------
 
     methods (Access=protected)
-
-        function [val] = getTdmsProperty(self, path, default)
-            if nargin < 3
-                default = [];
-            end
-
-            val = default;
-
-            data = self.tdmsStruct;
-            pathParts = strsplit(path, '/');
-            propName = pathParts{end};
-            for propPath = pathParts(1:end-1)
-                if ~isfield(data, propPath)
-                    return;
-                end
-                data = data.(propPath{:});
-            end
-
-            if isfield(data.Props, propName)
-                val = data.Props.(propName);
-            end
-        end
 
     end
 
